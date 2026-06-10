a06fc3c6b4f0ef89b74e81d11d53768cf9d86a88
Steve Forde <sforde08@gmail.com>
2026-06-10 02:44:41 +0100

    complete course step2 with exhaustive type checking and special type

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -1,8 +1,37 @@
-// 1. Structural interfaces
-interface CoursePart {
+// 1. Interfaces & Type Layout
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartBasic extends CoursePartDescription {
+  kind: "basic";
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group";
+}
+
+interface CoursePartBackground extends CoursePartDescription {
+  backgroundMaterial: string;
+  kind: "background";
+}
+
+interface CoursePartSpecial extends CoursePartDescription {
+  requirements: string[];
+  kind: "special";
+}
+
+type CoursePart =
+  | CoursePartBasic
+  | CoursePartGroup
+  | CoursePartBackground
+  | CoursePartSpecial;
 
 interface HeaderProps {
   name: string;
@@ -16,23 +45,75 @@ interface TotalProps {
   parts: CoursePart[];
 }
 
-// 2. Child Components
+// 2. Exhaustive Type Check Helper
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`,
+  );
+};
+
+// 3. Components
 const Header = (props: HeaderProps) => {
   return <h1>{props.name}</h1>;
+};
+
+const Part = ({ part }: { part: CoursePart }) => {
+  switch (part.kind) {
+    case "basic":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+        </p>
+      );
+    case "group":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          project exercises {part.groupProjectCount}
+        </p>
+      );
+    case "background":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+          <br />
+          submit to {part.backgroundMaterial}
+        </p>
+      );
+    case "special":
+      return (
+        <p>
+          <strong>
+            {part.name} {part.exerciseCount}
+          </strong>
+          <br />
+          <em>{part.description}</em>
+          <br />
+          required skills: {part.requirements.join(", ")}
+        </p>
+      );
+    default:
+      return assertNever(part);
+  }
 };
 
 const Content = (props: ContentProps) => {
   return (
     <div>
-      <p>
-        {props.parts[0].name} {props.parts[0].exerciseCount}
-      </p>
-      <p>
-        {props.parts[1].name} {props.parts[1].exerciseCount}
-      </p>
-      <p>
-        {props.parts[2].name} {props.parts[2].exerciseCount}
-      </p>
+      {props.parts.map((part, index) => (
+        <Part key={index} part={part} />
+      ))}
     </div>
   );
 };
@@ -42,24 +123,55 @@ const Total = (props: TotalProps) => {
     (sum, part) => sum + part.exerciseCount,
     0,
   );
-  return <p>Number of exercises {totalExercises}</p>;
+  return (
+    <p>
+      <strong>Number of exercises {totalExercises}</strong>
+    </p>
+  );
 };
 
-// 3. Main App Component
+// 4. Main App Controller
 const App = () => {
   const courseName = "Half Stack application development";
   const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
 
